09836a7995fb552656f5c764378dbce2511db57d
cjsherwin <chris.sherwin@kaluza.com>
2020-11-19 20:09:24 +0000

    Start Testing

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -6,7 +6,7 @@ import fpinscala.parallelism._
 import fpinscala.parallelism.Par.Par
 import Gen._
 import Prop._
-import java.util.concurrent.{Executors,ExecutorService}
+import java.util.concurrent.{Executors, ExecutorService}
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
@@ -14,22 +14,42 @@ shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
+
+//  def &&(p: Prop): Prop = new Prop {
+//    def check: Boolean = Prop.this.check && p.check
+//  }
 }
 
 object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
+
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
+case class Gen[A](sample: State[RNG,A])
+
 object Gen {
   def unit[A](a: => A): Gen[A] = ???
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
+  }
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
-}
+//trait Gen[A] {
+//  def map[A, B](f: A => B): Gen[B] = ???
+//
+//  def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
+//}
 
 trait SGen[+A] {
 
 }
 
+object Testing {
+  def main(args: Array[String]): Unit = {
+    println(s"Gen.choose(1, 10): ${Gen.choose(1, 10)}")
+  }
+}
